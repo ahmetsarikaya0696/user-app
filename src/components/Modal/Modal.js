43bb50c837a6f3df0523270621e7b1e3b19fc0c4
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-18 11:42:36 +0300

    react-dom, createPortal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,27 +1,44 @@
 import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
-const Modal = (props) => {
-  const clickHandler = () => {
-    props.onModalClose();
-  };
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onModalClose} />;
+};
 
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.errorMessage}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onModalClose}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const Modal = (props) => {
   return (
     <Fragment>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.errorMessage}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={clickHandler}>Ok</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onModalClose={props.onModalClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          errorMessage={props.errorMessage}
+          onModalClose={props.onModalClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
