c7861016d93694141835f90f25d8d4b3de92b1b8
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-18 11:13:16 +0300

    modal and card styling

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,19 +1,29 @@
 import React from "react";
 import styles from "./Modal.module.css";
 import Button from "../UI/Button";
+import Card from "../UI/Card";
 
 const Modal = (props) => {
   const clickHandler = () => {
     props.onModalClose();
   };
 
-  return <div className={styles.modal}>
-    <div className={styles.title}>{props.title}</div>
+  return (
     <div>
-      <p className={styles.errorMessage}>{props.errorMessage}</p>
-      <Button onClick={clickHandler} className={styles.btn}>Ok</Button>
+      <div className={styles.backdrop} onClick={props.onConfirm} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.errorMessage}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={clickHandler}>Ok</Button>
+        </footer>
+      </Card>
     </div>
-  </div>;
+  );
 };
 
 export default Modal;
